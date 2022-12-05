d82163fc07ca104d30b522bf4178400e0ab978db
watssupp <jorgesolisruiz@gmal.com>
2022-12-04 22:52:32 -0600

    added module to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -46,10 +46,27 @@ module.exports = () => {
         ],
       }),
     ],
-
+    
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        }
       ],
     },
   };
